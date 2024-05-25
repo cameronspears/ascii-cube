9f97acbba3fdabaae5dfd101f17284b47c678b5d
cameronspears <crspears@outlook.com>
2024-05-25 12:15:15 -0500

    file cleanup

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "851ac34f995c771e"; }
+/******/ 		__webpack_require__.h = function() { return "37ab1e3a822a75ad"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1216,11 +1216,6 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
-/******/ 	}();
-/******/ 	
-/******/ 	/* webpack/runtime/nonce */
-/******/ 	!function() {
-/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import CubeViewer from '../components/CubeViewer'; // Adjust the path as necessary
 
 export default function Home() {
-  return (
-    <div>
-      <CubeViewer />
-    </div>
-  );
+    return (
+        <div>
+            <CubeViewer/>
+        </div>
+    );
 }
